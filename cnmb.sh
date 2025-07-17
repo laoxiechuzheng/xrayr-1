6f71e7f7e5ffc47dcdb35dcbafd2e4e5761427f9
laoxiechuzheng <151037155+laoxiechuzheng@users.noreply.github.com>
2025-07-17 12:33:14 +0800

    Update cnmb.sh

--- cnmb.sh
+++ cnmb.sh
@@ -13,11 +13,11 @@ sudo systemctl restart sshd
 
 # 设置时区为上海
 timedatectl set-timezone Asia/Shanghai
-
+sleep 3
 # 关闭 systemd-resolved 并修改 DNS
 systemctl stop systemd-resolved && systemctl disable systemd-resolved
 rm -rf /etc/resolv.conf && echo 'nameserver 8.8.8.8' > /etc/resolv.conf
-
+sleep 3
 # 安装并运行 nyanpass 客户端
 S=nyanpass OPTIMIZE=1 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 81c0a326-cb00-47f6-80b7-19a1292d0f96 -u https://nbny.laoxiechuanmei.icu"
 
@@ -25,18 +25,12 @@ S=nyanpass OPTIMIZE=1 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-i
 # 安装并运行 nyanpass 客户端2
 S=nyanpass2 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-t 6a6bd450-6482-44a8-bf7c-9efc8e9e0003 -u https://nbny.laoxiechuanmei.icu"
 
-# 安装 dnsmasq + sniproxy
-wget --no-check-certificate -O dnsmasq_sniproxy.sh https://raw.githubusercontent.com/myxuchangbin/dnsmasq_sniproxy_install/master/dnsmasq_sniproxy.sh
-chmod +x dnsmasq_sniproxy.sh
-echo "" | bash dnsmasq_sniproxy.sh -id
 
-# 覆盖 dnsmasq 配置
-wget https://raw.githubusercontent.com/laoxiechuzheng/xrayr-1/refs/heads/main/awsjp-dns -O /etc/dnsmasq.conf
-rm -rf /etc/dnsmasq.d/custom_netflix.conf
-systemctl restart dnsmasq
 
 # 安装 XrayR
 wget -N https://raw.githubusercontent.com/wyx2685/XrayR-release/master/install.sh && bash install.sh
+
+sleep 5
 
 # 覆盖 rulelist 和配置
 wget https://raw.githubusercontent.com/laoxiechuzheng/shen-jishenji/main/LICENSE.md -O /etc/XrayR/rulelist
@@ -55,9 +49,23 @@ sudo systemctl start XrayR.service
 
 
 xrayr restart
+sleep 3
 
+# 安装 dnsmasq + sniproxy
+wget --no-check-certificate -O dnsmasq_sniproxy.sh https://raw.githubusercontent.com/myxuchangbin/dnsmasq_sniproxy_install/master/dnsmasq_sniproxy.sh
+chmod +x dnsmasq_sniproxy.sh
+echo "" | bash dnsmasq_sniproxy.sh -id
+
+sleep 5
+
+# 覆盖 dnsmasq 配置
+wget https://raw.githubusercontent.com/laoxiechuzheng/xrayr-1/refs/heads/main/awsjp-dns -O /etc/dnsmasq.conf
+sleep 2
+rm -rf /etc/dnsmasq.d/custom_netflix.conf
+systemctl restart dnsmasq
+sleep 3
 rm -rf /etc/resolv.conf && echo 'nameserver 127.0.0.1' > /etc/resolv.conf
-
+sleep 3
 
 # 安装 BBR 加速
 wget https://raw.githubusercontent.com/laoxiechuzheng/xrayr-1/refs/heads/main/dlsbbr.sh && bash dlsbbr.sh
