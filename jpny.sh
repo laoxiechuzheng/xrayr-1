daa11cef800daf6bd70b5a5f9e939f0945321fee
laoxiechuzheng <151037155+laoxiechuzheng@users.noreply.github.com>
2025-07-21 23:05:30 +0800

    Create jpny.sh

---
+++ jpny.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "========== 初始化配置开始 =========="
+
+
+# 安装并运行 nyanpass 客户端
+S=nyanpass OPTIMIZE=1 bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 6a6bd450-6482-44a8-bf7c-9efc8e9e0003 -u https://nbny.laoxiechuanmei.icu"
+
+
+
+
+echo "========== 初始化配置完成 =========="
